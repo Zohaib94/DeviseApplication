d4874d9f91ba2f1d79347ccbc1dccb8879943061
Zohaib <zohaibahmedbutt@gmail.com>
2016-06-17 09:32:09 +0500

    Installed Devise initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ DeviseApplication::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Configuring Default URL options for Action Mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
